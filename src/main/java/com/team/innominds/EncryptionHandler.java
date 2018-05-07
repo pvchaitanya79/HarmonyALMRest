bc4791d2e303566995dea5d19b67e970727cde07
chaitanya <cpinnamaraju@inoominds.com>
2018-05-07 12:37:15 +0530

    Signed-off-by: chaitanya <cpinnamaraju@innominds.com>

---
+++ src/main/java/com/team/innominds/EncryptionHandler.java
@@ -0,0 +1,77 @@
+package com.team.innominds;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class EncryptionHandler {
+	
+	private static SecretKeySpec secretKey;
+	private static byte[] key;
+
+	public static void setKey(String myKey) {
+		MessageDigest sha = null;
+		try {
+			key = myKey.getBytes("UTF-8");
+			sha = MessageDigest.getInstance("SHA-1");
+			key = sha.digest(key);
+			key = Arrays.copyOf(key, 16);
+			secretKey = new SecretKeySpec(key, "AES");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String encrypt(String strToEncrypt, String secret) {
+		try {
+			setKey(secret);
+			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+			return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+		} catch (Exception e) {
+			System.out.println("Error while encrypting: " + e.toString());
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		//args[1] -text to encrypt in double quotes
+		//System.out.println(args.length);
+
+		//if ((args.length==1)&&(!args[0].isEmpty())) {
+			String originalString = "test";
+			//args[2] -secret key (any arbitrary secret)
+			final String secretKey = "Innominds123$";
+			EncryptionHandler et = new EncryptionHandler();
+			
+			String encryptedString = et.encrypt(originalString, secretKey);
+			//String decryptedString = et.decrypt(encryptedString, secretKey);
+	
+			System.out.println("Original text is: "+originalString);
+			System.out.println("Encrypted Text is: "+encryptedString);
+			//System.out.println(decryptedString);
+//		} else {
+//			System.out.println("#Failed: Empty input or invalid number of arguments (MAX=1)");
+//		}
+	}
+
+	public static String decrypt(String strToDecrypt, String secret) {
+		try {
+			setKey(secret);
+			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+			cipher.init(Cipher.DECRYPT_MODE, secretKey);
+			return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+		} catch (Exception e) {
+			System.out.println("Error while decrypting: " + e.toString());
+		}
+		return null;
+	}
+
+}
---
+++ src/main/java/com/team/innominds/RESTAPIHandler.java
@@ -0,0 +1,183 @@
+package com.team.innominds;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Base64;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class RESTAPIHandler {	
+	static Logger logger = LoggerFactory.getLogger("com.team.innominds.RESTAPIHandler");
+	
+	public static void main(String[] args) {
+		CloseableHttpClient httpclient = HttpClients
+                .custom()
+                .build();
+	    logger.debug("Hello world.");
+	    String projectName="901637940_DEMO";
+	    String domainName="DEFAULT_901637940";
+		String almAuthUrl = "https://almalm1250saastrial.saas.hpe.com/qcbin/api/authentication/sign-in";
+		String almUrl = "https://almalm1250saastrial.saas.hpe.com/qcbin/rest/";
+		String userName = "cpinnamaraju_innominds.com";
+		final String secretKey = "Innominds123$";	
+		//String encryptedString = et.encrypt(password, secretKey);
+		String encryptedString = "LnsFoVq5OSoJ0C+ar5Z8SQ==";
+		RESTAPIHandler restHandle = new RESTAPIHandler(); 
+		restHandle.almAuthenticate(httpclient, almAuthUrl, userName, EncryptionHandler.decrypt(encryptedString, secretKey));
+		
+		//This part of the code gets triggered from Harmony Framework
+		try {
+			//Read mapping file and update test status for each test
+			BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir")+"src/main/resources/mapping.csv"));
+			String line = br.readLine();
+			while (line!=null) {
+				String[] tokens = line.split(",");
+				restHandle.updateTestStatus(httpclient, almUrl+"domains/"+domainName+"/projects/"+projectName+"/", tokens[1], "Passed");
+				line = br.readLine();
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			logger.error("File Not found Exception: "+e.getMessage());
+		} catch (IOException e) {
+			logger.error("IO Exception: "+e.getMessage());
+		}		
+		restHandle.cleanUp(httpclient);  
+		
+	}
+	
+	private void updateTestStatus(CloseableHttpClient httpclient, String almUrl, String testID, String tstatus) {
+		// TODO Auto-generated method stub
+		String tesinstanceQueryURL = almUrl + "test-instances?query={test-id["+testID+"]}";
+		try {             
+        	HttpUriRequest request1 = RequestBuilder.get()
+                    .setUri(tesinstanceQueryURL)
+                    .setHeader(HttpHeaders.CONTENT_TYPE, "text/xml")                 
+                    .build();
+            ResponseHandler<String> responseHandler1 = response -> {
+                int status = response.getStatusLine().getStatusCode();
+                if (status >= 200 && status < 400) {                
+                	HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
+                }
+            };
+            String responseBody = "";
+            responseBody = httpclient.execute(request1, responseHandler1);
+			logger.info("Test Instance Query success with response: "+responseBody);
+			InputSource source = new InputSource(new StringReader(responseBody));
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document document = db.parse(source);
+			
+			XPathFactory xpathFactory = XPathFactory.newInstance();
+			XPath xpath = xpathFactory.newXPath();
+			ArrayList<StringBuilder> runXMLData = new ArrayList<StringBuilder>();
+			runXMLData.add(new StringBuilder(xpath.evaluate("//Field[@Name=\"test-config-id\"]", document)));
+			runXMLData.add(new StringBuilder(xpath.evaluate("//Field[@Name=\"cycle-id\"]", document)));
+			runXMLData.add(new StringBuilder(testID));			
+			runXMLData.add(new StringBuilder(xpath.evaluate("//Field[@Name=\"id\"]", document)));
+			//Arbitrary run name
+			runXMLData.add(new StringBuilder("HarmonyRun1"));
+			runXMLData.add(new StringBuilder(xpath.evaluate("//Field[@Name=\"owner\"]", document)));
+			runXMLData.add(new StringBuilder(tstatus));
+			String execution_date = "2018-05-07";
+			runXMLData.add(new StringBuilder(execution_date));
+			String execution_time = "11:55:00";
+			runXMLData.add(new StringBuilder(execution_time));
+			
+			String runXML = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>	<Entity Type=\"run\">	<Fields>	<Field Name=\"test-config-id\"><Value>##param1</Value></Field>	<Field Name=\"cycle-id\"><Value>##param2</Value></Field>	<Field Name=\"test-id\"><Value>##param3</Value></Field>	<Field Name=\"testcycl-id\"><Value>##param4</Value></Field>	<Field Name=\"build-revision\"><Value>1</Value></Field>	<Field Name=\"name\"><Value>##param5</Value></Field>	<Field Name=\"owner\"><Value>##param6</Value></Field>	<Field Name=\"status\"><Value>##param7</Value></Field>	<Field Name=\"subtype-id\"><Value>hp.qc.run.external-test</Value></Field>	<Field Name=\"duration\"><Value>5</Value></Field>	<Field Name=\"execution-date\"><Value>##param8</Value></Field>	<Field Name=\"execution-time\"><Value>##param9</Value></Field>	<Field Name=\"status\"><Value>##param7</Value></Field>	</Fields>	</Entity>";
+			for (int i=1; i<=9; i++) {
+				runXML = runXML.replaceAll("##param"+i, runXMLData.get(i).toString());
+			}
+            String runUrl = almUrl + "runs";
+            //HTTP POST here
+            HttpUriRequest request2 = RequestBuilder.post()
+                    .setUri(runUrl)
+                    .setHeader(HttpHeaders.CONTENT_TYPE, "text/xml")                 
+                    .build();
+            ResponseHandler<String> responseHandler2 = response -> {
+                int status = response.getStatusLine().getStatusCode();
+                if (status >= 200 && status < 400) {                
+                	HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
+                }
+            };
+            String responseBody1 = "";
+            responseBody1 = httpclient.execute(request2, responseHandler2);
+			logger.info("ALM Test Status Update successful: "+responseBody1);
+        } catch (IOException e1) {
+        	logger.error("Test Instance Query failure: "+e1.getMessage());
+		} catch (ParserConfigurationException e) {
+			logger.error("Parse failure: "+e.getMessage());
+		} catch (XPathExpressionException e) {
+			logger.error("XPath Expression failure: "+e.getMessage());
+		} catch (SAXException e) {
+			logger.error("SAX Exception: "+e.getMessage());
+		}
+	}
+
+	public void cleanUp(CloseableHttpClient httpclient) {
+		try {
+			httpclient.close();
+		} catch (IOException e) {
+			logger.error("HTTP Client connection close failed: "+e.getMessage());
+		}
+	}
+	
+	//Trigger a Jenkins job
+	public boolean almAuthenticate(CloseableHttpClient httpclient, String almAuthUrl, String userName, String password) {
+        boolean success = false;
+        String auth = userName+":"+password;
+        String encodedauth = "Basic "+Base64.getEncoder().encodeToString(auth.getBytes());        
+        try {             
+            //Authenticate
+        	HttpUriRequest request1 = RequestBuilder.get()
+                    .setUri(almAuthUrl)
+                    .setHeader(HttpHeaders.CONTENT_TYPE, "text/xml")
+                    .setHeader(HttpHeaders.AUTHORIZATION, encodedauth)
+                    .build();
+            ResponseHandler<String> responseHandler1 = response -> {
+                int status = response.getStatusLine().getStatusCode();
+                if (status >= 200 && status < 400) {                
+                	HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
+                }
+            };
+            String responseBody = "";
+            responseBody = httpclient.execute(request1, responseHandler1);
+			logger.info("Authentication with API successful and respense is: "+responseBody);
+			success = true;
+            
+        } catch (IOException e1) {
+        	logger.error("Authentication failure: "+e1.getMessage());
+		}            
+        return success;
+	}
+}
